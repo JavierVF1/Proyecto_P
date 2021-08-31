9beead14b7e8d67d3abd1e399f9e25cc2de63d98
LeDavinshi <jorge110502@gmail.com>
2021-08-31 17:49:52 -0400

    Update ProyectoP.py

--- ProyectoP.py
+++ ProyectoP.py
@@ -2,7 +2,9 @@
 import pygame
 import sys
 import time
-"""davinshi estuvo aquí"""
+
+"""bingo bango bongo i don't wanna leave the congo o no no no no no"""
+
 ejecutando = True
 frameRate = 60
 frameCount = 0
