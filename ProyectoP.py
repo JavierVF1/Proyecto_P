ca0f0d58b781b22c520ff5551814ad8d97397025
JavierVF1 <89874445+JavierVF1@users.noreply.github.com>
2021-08-31 16:38:54 -0400

    Add files via upload

---
+++ ProyectoP.py
@@ -0,0 +1,37 @@
+ 
+import pygame
+import sys
+import time
+ 
+ejecutando = True
+frameRate = 60
+frameCount = 0
+ 
+pygame.init()
+ 
+canvas = pygame.display.set_mode([800, 600],pygame.RESIZABLE)
+ 
+def events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            global ejecutando
+            ejecutando = False
+ 
+ 
+def update():
+    events()
+    pygame.display.flip()
+    time.sleep(1/frameRate)
+    global frameCount
+    frameCount += 1
+ 
+ 
+def draw():
+    pygame.draw.rect(canvas, [frameCount % 255, 0, 0], [0, 0, 800, 600])
+ 
+while ejecutando:
+    draw()
+    update()
+ 
+sys.exit()
+ 
