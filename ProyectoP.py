3a8e1d3e27a0cce379d5e83c4b24b68ac24a448e
JavierVF1 <89874445+JavierVF1@users.noreply.github.com>
2021-09-01 02:33:10 -0400

    Update ProyectoP.py

--- ProyectoP.py
+++ ProyectoP.py
@@ -1,15 +1,25 @@
- 
 import pygame
 import sys
 import time
-
+from math import pi
+ 
 ejecutando = True
 frameRate = 60
 frameCount = 0
+
  
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+BLUE =  (  0,   0, 255)
+GREEN = (  0, 255,   0)
+RED =   (255,   0,   0)
+
+
 pygame.init()
  
-canvas = pygame.display.set_mode([800, 600],pygame.RESIZABLE)
+canvas = pygame.display.set_mode([850, 600],pygame.RESIZABLE)
+pygame.display.set_caption("Age of tanks")
+
  
 def events():
     for event in pygame.event.get():
@@ -17,6 +27,7 @@ def events():
             global ejecutando
             ejecutando = False
  
+
 def update():
     events()
     pygame.display.flip()
@@ -24,12 +35,49 @@ def update():
     global frameCount
     frameCount += 1
  
+ 
 def draw():
-    pygame.draw.rect(canvas, [frameCount % 255, 0, 200], [0, 0, 800, 600])
+
+
+    pygame.draw.rect(canvas, WHITE, [0, 0, 850, 600])
+    pygame.draw.rect(canvas, GREEN, [0, 500, 850, 600])
+
+    pygame.draw.line(canvas, GREEN, [0, 500], [103,500], 5)
+    
+    pygame.draw.arc(canvas, GREEN,[100,440, 150, 125], pi/2, pi, 100)
+    pygame.draw.arc(canvas, GREEN,[100,440, 150, 125], 0, pi/2, 100)
+
+    pygame.draw.line(canvas, GREEN, [300,500], [250,500], 5)
+
+    pygame.draw.arc(canvas, WHITE, [300,453, 100, 90], pi,3*pi/2, 100)
+    pygame.draw.arc(canvas, WHITE,  [300,453, 100, 90], 3*pi/2, 2*pi, 100)
+
+    pygame.draw.line(canvas, GREEN, [400,500], [450,500], 5)
+
+    pygame.draw.arc(canvas, WHITE, [450,453, 100, 90], pi,3*pi/2, 100)
+    pygame.draw.arc(canvas, WHITE,  [450,453, 100, 90], 3*pi/2, 2*pi, 100)
+    
+    pygame.draw.line(canvas, GREEN, [550,500], [605,500], 5)
+
+    pygame.draw.arc(canvas, GREEN,[600,440, 150, 125], pi/2, pi, 100)
+    pygame.draw.arc(canvas, GREEN,[600,440, 150, 125], 0, pi/2, 100)
+
+    pygame.draw.line(canvas, GREEN, [750, 500], [850,500], 5)
+    
+  
+    
+
  
 while ejecutando:
     draw()
     update()
+    while True:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                sys.exit()
+    
+
 
 class tank:
     #atributos
@@ -80,6 +128,8 @@ class tank:
         self.color=color
         
     #metodos
+
+
 
 sys.exit()
  
