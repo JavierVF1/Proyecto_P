09ed3c1e77c7f1e6f9bb74fdfdd6571b681888b0
LeDavinshi <jorge110502@gmail.com>
2021-09-02 23:56:14 -0400

    Tank and arena
    Creating a tank and arena to test pygame

--- main.py
+++ main.py
@@ -6,36 +6,58 @@ pygame.init()
 size = 800, 600
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Panzerquack")
+#screen size
 width=800
 height=600
+
+"""
+esto tiene que poderse modificar, para poder hacer que en un futuro
+se pueda usarse la pantalla completa
+JR
+"""
+#fundamental forces and tank movement
 speed=[1,1]
+gravity=[0,5]
+
+#background
 white=255,255,255
+blue_sky=135,206,235
+
+
+#player
 tank=pygame.image.load("assets\sprites\PLAYERS\GREEN_P\duck_s.png")
 tankrect=tank.get_rect()
-tankrect.move_ip(400,300)
+tankrect.move_ip(400,100)
+
+#map
+floor=pygame.image.load("assets/maps/cp_orange/floor.png")
+floorect=floor.get_rect()
+floorect.move_ip(0,500)
+
 run=True
 while run:
     pygame.time.delay(2)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: run=False
     keys=pygame.key.get_pressed()
+    #movement
     if keys[pygame.K_LEFT]:
-        tank=pygame.image.load("assets\sprites\PLAYERS\GREEN_P\duck_m.png")
-        pygame.time.delay(1)
-        tank=pygame.image.load("assets\sprites\PLAYERS\GREEN_P\duck_m2.png")
         tankrect=tankrect.move(-1,0)
-    else:
-        tank=pygame.image.load("assets\sprites\PLAYERS\GREEN_P\duck_s.png")
     if keys[pygame.K_RIGHT]:
-        tank=pygame.image.load("assets\sprites\PLAYERS\GREEN_P\duck_m.png")
-        pygame.time.delay(1)
-        tank=pygame.image.load("assets\sprites\PLAYERS\GREEN_P\duck_m2.png")
         tankrect=tankrect.move(1,0)
-    else:
-        tank=pygame.image.load("assets\sprites\PLAYERS\GREEN_P\duck_s.png")
-    if tankrect.left < 0 or tankrect.right>width:
+    #boundaries
+    if tankrect.left < 0:
+        tankrect=tankrect.move(1,0)
+    if tankrect.right>width:
+        tankrect=tankrect.move(-1,0)
+    #floor collide
+    if floorect.colliderect(tankrect):
         tankrect=tankrect.move(0,0)
-    screen.fill(white)
+    #gravity
+    else:
+        tankrect=tankrect.move(gravity)
+    screen.fill(blue_sky)
+    screen.blit(floor,floorect)
     screen.blit(tank,tankrect)
     pygame.display.flip()
     
