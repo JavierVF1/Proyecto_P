3019dbd5062fbd97bebbf852a813a27fbe47ea78
LeDavinshi <jorge110502@gmail.com>
2021-09-15 17:16:59 -0300

    Modificación minima
    ncesito que alguien me ayude a poder crear los objetos map, y 2 objetos tank, luego poerlos ingresar al mapa y ponerlos en pantalla

--- Panzerquack v0.2.py
+++ Panzerquack v0.2.py
@@ -9,7 +9,7 @@ Created on Tue Sep  7 18:19:21 2021
 import pygame
 import math
 
-keys=pygame.key.get_pressed()
+
 time=0
 #########################  SHELL #########################
 class Shell(pygame.sprite.Sprite):
@@ -20,7 +20,7 @@ class Shell(pygame.sprite.Sprite):
         self.x=x
         self.y=y
 #########################  TORRETA #########################
-class Turret(pygame.sprite.Sprite,Shell):
+class Turret(pygame.sprite.Sprite):
     def __init__(self,imagen):
         self.imagen=imagen
         self.rect=self.imagen.get_rect()
@@ -37,22 +37,19 @@ class Turret(pygame.sprite.Sprite,Shell):
         self.ang=ang
         self.vel=vel
 #########################  TANQUE #########################
-class Tank(pygame.sprite.Sprite,Turret):
-    def __init__(self,imagen,spawn,turno):
-        self.imagen=imagen
-        self.rect=self.imagen.get_rect()
-        self.spawn=spawn
-        self.turno
-        #tamaño
-        self.rect.top=100
-        self.rect.left=200
-    def spawn(self,spawn):
-        self.rect.move_ip(spawn)
-    def pos(self,x,y):
+class Tank(pygame.sprite.Sprite):
+    def __init__(self,x,y,width,height,imagen,turno):
         self.x=x
         self.y=y
-    def update(self,superficie):
-        superficie.blit(self.imagen,self.rect)
+        self.width=width
+        self.height=height
+        self.imagen=imagen
+        self.rect=self.imagen.get_rect()
+        self.turno=turno
+
+    def spawn(self,x,y):
+        self.rect.move_ip(x,y)
+
     #codigo para despues, cuando pidan movimiento será util ~JR
     # def boundaries(self):
     #     if self.rect.move <0:
@@ -61,8 +58,8 @@ class Tank(pygame.sprite.Sprite,Turret):
     #         self.rect.move(-2,0)
         
 #########################  MAPA #########################
-class g_map(pygame.sprite.Sprite,Tank):
-    def __init__(self,imagen,f_gravedad,turno_map):
+class Map(pygame.sprite.Sprite):
+    def __init__(self,imagen,gravity,turno_map):
         self.imagen=imagen
         self.rect=self.imagen.get_rect()
         self.turno_map=turno_map
@@ -81,14 +78,27 @@ class g_map(pygame.sprite.Sprite,Tank):
 
 def main():
     import pygame
-    
     pygame.init()
-    pantalla=pygame.display.set_mode((100,600))
-    salir=False
-    imagen=pygame.image.load("assets\sprites\PLAYERS\GREEN_P\duck_s.png")
+    pantalla=pygame.display.set_mode((600,600))
     
-    tank_r=Tank(imagen)
+    #tank obj
+    #ejemplo para futuro:
+    #tank_x=pygame.image.load("assets\Sprites\PLAYERS\X_P\duck_s.png)
+    #El comando de arriba quiere decir que se cargara la imagen duck_s (duck still), que se usara...
+    #en la creación del tanque
+    #tank_xc=Tank(x,y,width,height,tank_x,turno)
+    #este de aquí arriba es el objeto tank
+    #tiene coordenadas x e y que serán su punto de spawn
+    #un ancho y alto
+    #un sprite tank_x
+    #y finalmente un numero de turno que debiese ser modificado manualmente
+        #tank green
+    tank_g=pygame.image.load("assets\sprites\PLAYERS\GREEN_P\duck_s.png")
+    tank_gc=Tank(500,100,10,10,tank_g,1)
+        #tank red
+    # tank_r=pygame.image.load("assets\sprites\PLAYERS\RED_P\duck_s.png")
+    tank_rc=Tank(500,500,10,10,tank_g,2)
     pantalla.fill((200,200,200))
-    tank_r.update(pantalla)
+    pantalla.blit(tank_gc.imagen,tank_gc.rect)
     pygame.display.update()
 main()
