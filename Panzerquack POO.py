9ef955189cdf281590c7303d5578df64d66911b0
JavierVF1 <89874445+JavierVF1@users.noreply.github.com>
2021-11-02 19:09:57 -0300

    mejora interfaz

--- Panzerquack POO.py
+++ Panzerquack POO.py
@@ -5,6 +5,157 @@ import time
 from random import randint,choice
 from pygame.locals import *
 
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.running, self.playing = True, False
+        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+        self.DISPLAY_W, self.DISPLAY_H = 800, 600
+        self.display = pygame.Surface((self.DISPLAY_W,self.DISPLAY_H))
+        self.window = pygame.display.set_mode(((self.DISPLAY_W,self.DISPLAY_H)))
+        self.font_name = '8-BIT WONDER.TTF'
+        #self.font_name = pygame.font.get_default_font()
+        self.BLACK, self.WHITE = (0,160,235), (0,70,70)
+        self.main_menu = MainMenu(self)
+        #self.options = OptionsMenu(self)
+        self.exit = ExitMenu(self)
+        self.curr_menu = self.main_menu
+
+    def game_loop(self):
+        while self.playing:
+            self.check_events()
+            if self.START_KEY:
+                self.playing= False
+            self.display.fill(self.BLACK)
+            self.draw_text('Thanks for Playing', 20, self.DISPLAY_W/2, self.DISPLAY_H/2)
+            self.window.blit(self.display, (0,0))
+            pygame.display.update()
+            self.reset_keys()
+            g.running=False
+            break
+        return
+            
+
+
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running, self.playing = False, False
+                self.curr_menu.run_display = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    self.START_KEY = True
+                if event.key == pygame.K_BACKSPACE:
+                    self.BACK_KEY = True
+                if event.key == pygame.K_DOWN:
+                    self.DOWN_KEY = True
+                if event.key == pygame.K_UP:
+                    self.UP_KEY = True
+
+    def reset_keys(self):
+        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+
+    def draw_text(self, text, size, x, y ):
+        font = pygame.font.Font(self.font_name,size)
+        text_surface = font.render(text, True, self.WHITE)
+        text_rect = text_surface.get_rect()
+        text_rect.center = (x,y)
+        self.display.blit(text_surface,text_rect)
+
+class Menu():
+    def __init__(self, game):
+        self.game = game
+        self.mid_w, self.mid_h = self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2
+        self.run_display = True
+        self.cursor_rect = pygame.Rect(0, 0, 20, 20)
+        self.offset = - 100
+
+    def draw_cursor(self):
+        self.game.draw_text('*', 15, self.cursor_rect.x, self.cursor_rect.y)
+
+    def blit_screen(self):
+        self.game.window.blit(self.game.display, (0, 0))
+        pygame.display.update()
+        self.game.reset_keys()
+
+class MainMenu(Menu):
+    def __init__(self, game):
+        Menu.__init__(self, game)
+        self.state = "Start"
+        self.startx, self.starty = self.mid_w, self.mid_h + 30
+        self.optionsx, self.optionsy = self.mid_w, self.mid_h + 50
+        self.Exitx, self.Exity = self.mid_w, self.mid_h + 50
+        self.Panzerx, self.Panzery = self.mid_w, self.mid_h -150
+        self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
+
+    def display_menu(self):
+        self.run_display = True 
+        while self.run_display:
+            self.game.check_events()
+            self.check_input()
+            self.game.display.fill(self.game.BLACK)
+            #self.game.draw_text('Main Menu', 20, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 20)
+            self.game.draw_text("Start Game", 20, self.startx, self.starty)
+            self.game.draw_text("Panzerquack", 50, self.Panzerx, self.Panzery)
+            self.game.draw_text("Exit", 20, self.Exitx, self.Exity)
+            self.draw_cursor()
+            self.blit_screen()
+
+    def move_cursor(self):
+        if self.game.DOWN_KEY:
+            if self.state == 'Start':
+                self.cursor_rect.midtop = (self.optionsx + self.offset, self.optionsy)
+                self.state = 'Exit'
+            elif self.state == 'Exit':
+                self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
+                self.state = 'Start'
+        elif self.game.UP_KEY:
+            if self.state == 'Start':
+                self.cursor_rect.midtop = (self.Exitx + self.offset, self.Exity)
+                self.state = 'Exit'
+            elif self.state == 'Exit':
+                self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
+                self.state = 'Start'
+            
+
+   
+    def check_input(self):
+        self.move_cursor()
+        if self.game.START_KEY:
+            if self.state == 'Start':
+                self.game.playing = True
+            elif self.state == 'Exit':
+                self.game.curr_menu = self.game.Exit
+            self.run_display = False
+
+
+
+class ExitMenu(Menu):
+    def __init__(self, game):
+        Menu.__init__(self, game)
+
+    def display_menu(self):
+        self.run_display = True
+        while self.run_display:
+            self.game.check_events()
+            if self.game.START_KEY or self.game.BACK_KEY:
+                self.game.curr_menu = self.game.main_menu
+                self.run_display = False
+            self.game.display.fill(self.game.BLACK)
+            self.game.draw_text('Thanks for Playing', 20, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 20)
+            #self.game.draw_text('Made by me', 15, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 + 10)
+            self.blit_screen()
+            time.sleep(0.01)
+            break
+        exit()
+
+        
+
+
+
+
 class World():
     def __init__(self, data):
         self.tile_list = []
@@ -555,6 +706,14 @@ def textbox():
         if aux2==1:
             return aux
  
+g = Game()
+
+while g.running:
+    g.curr_menu.display_menu()
+    g.game_loop()
+    
+
+
 pygame.init()
 #PANTALLA
 screen_width = 800
@@ -576,6 +735,7 @@ world_data=MapaSelect(mapa)
 world = World(world_data)
 screen.blit(fondo, (0, 0))
 world.draw()
+#draw_grid()
 #IMAGES
 
 
@@ -683,8 +843,8 @@ bala90_2=3
 
 #Variables auxiliares
 run = True   #Variable while principal
-auxT=True   #Variable Pantalla de inicio (texto de inicio panzerquak)
-turno=0     #Variable control de turnos
+auxT=0   #Variable Pantalla de inicio (texto de inicio panzerquak)
+turno=10     #Variable control de turnos
 win=True    #Variable control de victoria
 
 while run:
@@ -698,21 +858,17 @@ while run:
 
     
 
-    
-
-    #draw_grid()
-
+    #screen.blit(fondo, (0, 0))
+    #world.draw()
     player1.update()
-
-    if auxT == True:  text()
-
-    if turno==10: turno=1
+  
+  
     
-    if keys[pygame.K_SPACE]: turno = 10
-
-    if turno!= 0: auxT=False
-
+    
+    
+        
     if turno == 2:
+        
         print("Turno DOS")
         while True:
             textvidap2 = texto11.render("Vida: "+str(player2.vida), 0, negro)
@@ -787,6 +943,7 @@ while run:
             run=False
 
     if turno == 1:
+        
         print("Turno UNO")
         screen.blit(img_right,(screen_width-40,screen_height-50))
         screen.blit(turn_text,(screen_width-120,screen_height-55))
@@ -860,5 +1017,9 @@ while run:
             #victoria()
             run=False
     pygame.display.update()
+
+    if turno==10:
+        turno=1
+    
 
 pygame.quit()
